15a8de04cda543897fd886aab893f10899345044
Max <maksym.kostenko1@nure.ua>
2022-07-06 13:23:35 +0300

    Bugs fixed

--- resources/views/includes/user/navbar.blade.php
+++ resources/views/includes/user/navbar.blade.php
@@ -53,7 +53,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </nav>
 </div>
@@ -61,6 +60,7 @@
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
--- storage/framework/views/6aa464c8a93205f9e7208d7d0b48cf5b1753f656.php
+++ storage/framework/views/6aa464c8a93205f9e7208d7d0b48cf5b1753f656.php
@@ -55,7 +55,6 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </nav>
 </div>
@@ -63,6 +62,7 @@
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 
